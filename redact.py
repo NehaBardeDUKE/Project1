3833e0fbcb684c5ab8d16d9d2597cd25de58b345
NehaBardeDUKE <110474064+NehaBardeDUKE@users.noreply.github.com>
2023-01-21 13:22:22 -0500

    Update redact.py

--- redact.py
+++ redact.py
@@ -1,1 +1,17 @@
 
+import re
+
+def redact_sensitive_info(document: str, keywords: list) -> str:
+    """
+    Redacts sensitive information from a document using a list of keywords
+    """
+    for keyword in keywords:
+        pattern = re.compile(r"\b" + keyword + r"\b")
+        document = pattern.sub("[REDACTED]", document)
+    return document
+
+document = "This is a document containing sensitive information such as email addresses and phone numbers."
+keywords = ["email", "phone", "address"]
+
+redacted_document = redact_sensitive_info(document, keywords)
+print(redacted_document)
